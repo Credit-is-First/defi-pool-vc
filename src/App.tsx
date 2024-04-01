fcec454def9b084a03f2a3fc64686cfff77f84f0
Credit-is-First <123669465+Credit-is-First@users.noreply.github.com>
2024-04-01 00:06:21 -0700

    Add react/typescript, code formatting with eslint, prettier, husky

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className='text-xl font-bold text-center mt-5'>Simple React Typescript Tailwind Sample</h1>
+  )
+}
+
+export default App
