39a2420a25a85e9a97d33aa2265ff3a887b4417d
Credit-is-First <123669465+Credit-is-First@users.noreply.github.com>
2024-04-03 15:26:13 -0700

    Update signup dialog with update base components

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -10,18 +10,29 @@ function SignupDialog({ className, ...others }: BaseDialogProps) {
       <h4 className='mx-[17px]'>Create a new account</h4>
       <div className='mt-[67px] rounded-[50px] bg-card py-[46px] pl-[54px]'>
         <div className='grid grid-cols-2 gap-[30px] w-[630px]'>
-          <BaseRoundedInput className='col-span-1 px-[30px] text-sm' placeholder='First name' />
-          <BaseRoundedInput className='col-span-1 px-[30px] text-sm' placeholder='Last name' />
           <BaseRoundedInput
-            className='col-span-2 px-[30px] text-sm'
+            className='col-span-1'
+            inputClass='px-[30px] text-sm'
+            placeholder='First name'
+          />
+          <BaseRoundedInput
+            className='col-span-1'
+            inputClass='px-[30px] text-sm'
+            placeholder='Last name'
+          />
+          <BaseRoundedInput
+            className='col-span-2'
+            inputClass='px-[30px] text-sm'
             placeholder='Your email address'
           />
           <BaseRoundedInput
-            className='col-span-2 px-[30px] text-sm'
+            className='col-span-2'
+            inputClass='px-[30px] text-sm'
             placeholder='Password ( 10 character minimum)'
           />
           <BaseRoundedInput
-            className='col-span-2 px-[30px] text-sm'
+            className='col-span-2'
+            inputClass='px-[30px] text-sm'
             placeholder='Repeat password'
           />
         </div>
